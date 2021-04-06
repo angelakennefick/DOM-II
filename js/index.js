2431492d46c816c11512d548b0e012cf1159f45a
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-04-06 16:37:47 -0500

    Got one!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+Array.from(document.links).forEach((link) =>{
+    link.addEventListener('click', (event) =>{
+        event.preventDefault();
+    })
+})
+
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener('mouseover', () =>{
+document.body.style.background = 'salmon'
+});
