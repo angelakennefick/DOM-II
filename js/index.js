e70f068cb25caadfcf1a3cd6f88e55f322bfbd84
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-04-06 18:54:57 -0500

    Added nav link transform

--- js/index.js
+++ js/index.js
@@ -26,10 +26,33 @@ contentSection.addEventListener('mouseover', () => {
  document.body.style.cursor = "pointer";
 });  
     
-  
-       
+//  6
+window.addEventListener('load', () => {
+    logo.style.transform = "skewX(20deg)"
+});
 
- 
+// 7
+let navLinks = document.querySelectorAll(".nav a");
+
+navLinks[0].addEventListener('mouseover', () => {
+    navLinks[0].style.transform ="skewY(20deg)"
+   });  
+
+// 8
+navLinks[1].addEventListener('mouseover', () => {
+    navLinks[1].style.transform ="rotate(90deg)"
+   });  
+// 9
+navLinks[2].addEventListener('mouseover', () => {
+    navLinks[2].style.transform ="scale(5, 10)"
+   });  
+// 10
+navLinks[3].addEventListener('mouseover', () => {
+    navLinks[3].style.transform ="translate(50px, 100px)"
+   });  
+
+
+
     
     
  
