0d8fe0fec316a10da2fc8ce028e365c8602f6404
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-04-06 16:55:50 -0500

    Two more

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
-Array.from(document.links).forEach((link) =>{
-    link.addEventListener('click', (event) =>{
-        event.preventDefault();
-    })
-})
+// Array.from(document.links).forEach((link) =>{
+//     link.addEventListener('click', (event) =>{
+//         event.preventDefault();
+//     })
+// })
 
+// 1
 const logo = document.querySelector(".logo-heading");
 logo.addEventListener('mouseover', () =>{
 document.body.style.background = 'salmon'
 });
+
+// 2
+logo.addEventListener('click', () =>{
+document.body.style.background = 'white'
+});
+
+// 3
+logo.addEventListener('dblclick', () =>{
+    logo.style.color = 'salmon';
+    });
